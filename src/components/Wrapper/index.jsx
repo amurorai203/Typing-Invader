7396cf0aedcf259b9a804cdf72dc48580a99e2b0
amurorai203 <71173161+amurorai203@users.noreply.github.com>
2024-01-23 20:09:47 +0000

    fix wrapper to cater table in Highscore

--- src/components/Wrapper/index.jsx
+++ src/components/Wrapper/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 // import "./style.css";
 
 function Wrapper(props) {
-  return <tbody className="wrapper">{props.children}</tbody>;
+  return <tbody className="wrapper">{props.children}</>;
 }
 
 export default Wrapper;
