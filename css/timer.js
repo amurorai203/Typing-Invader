55e33c8a5d04b9676372ac64aa78aea68a215969
Phoenixcoimbra <igormcoimbra@gmail.com>
2024-01-23 20:19:20 +0000

    timer react

--- css/timer.js
+++ css/timer.js
@@ -1,12 +1,22 @@
-// timer of 2 minutes
-var timeleft = 120;
-var downloadTimer = setInterval(function(){
-  document.getElementById("timer").innerHTML = timeleft + " seconds remaining";
-  timeleft -= 1;
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("timer").innerHTML = "Finished"
-  }
-}, 1000);
+import React, { useEffect, useState } from 'react';
+import './css/style.css';
 
+function Timer() {
+    const [timeLeft, setTimeLeft] = useState(120);
 
+    useEffect(() => {
+        if(timeLeft > 0) {
+            const timerId = setInterval(() => {
+                setTimeLeft(timeLeft - 1);
+            }, 1000);
+
+            return () => clearInterval(timerId);
+        }
+    }, [timeLeft]);
+
+    return (
+        <div id="timer">{timeLeft > 0 ? `${timeLeft} seconds remaining` : 'Finished'}</div>
+    );
+}
+
+export default Timer;
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// App.tsx
+import React from 'react';
+import Timer from './Timer';
+
+function App() {
+    return (
+        <div>
+            <Timer />
+            {/* Other components can go here */}
+        </div>
+    );
+}
+
+export default App;
