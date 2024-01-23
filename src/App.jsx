0967639b4940cb4bc7fb76dee258ec1d047b6c1e
pineapple laptop <martinkosel@yahoo.de>
2024-01-23 15:28:57 +0000

    Fixed file structure and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import StartGame from './components/pages/startGame';
 import HighScores from './components/pages/scorelist';
 import MenuItem from './components/MenuItem';
